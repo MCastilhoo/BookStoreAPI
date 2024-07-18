28ea197009293ac9f8a5407851376679004b0877
Matheus Castilho <dev.mcastilho@gmail.com>
2024-07-18 08:31:52 -0300

    Adding RoleRepository in repositories

---
+++ src/main/java/com/br/BookStoreAPI/repositories/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.br.BookStoreAPI.repositories;
+
+import com.br.BookStoreAPI.models.entities.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
+    RoleEntity findByName(String name);
+}
