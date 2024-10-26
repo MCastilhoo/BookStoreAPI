b614e32350620a49188d3658ede22ed983344594
Matheus Castilho <dev.mcastilho@gmail.com>
2024-10-26 06:38:24 -0300

    Switch UUID to Long in BookRepository

--- src/main/java/com/br/BookStoreAPI/repositories/BookRepository.java
+++ src/main/java/com/br/BookStoreAPI/repositories/BookRepository.java
@@ -4,8 +4,6 @@ import com.br.BookStoreAPI.models.entities.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface BookRepository extends JpaRepository<BookEntity, UUID> {
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
 }
