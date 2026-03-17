af2e46c2fb95b4743f7c2c286aa07b352246814f
Matheus Castilho <dev.mcastilho@gmail.com>
2026-03-17 15:39:47 -0300

    fix: The route for details in the book has been changed from private to public.

--- src/main/java/com/br/BookStoreAPI/config/SecurityConfig.java
+++ src/main/java/com/br/BookStoreAPI/config/SecurityConfig.java
@@ -74,6 +74,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/genres").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/books").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/books/{title}").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/books/livro/{slug}").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
